f4151144e7e593f270ece4c3fbcb70c950dbc52d
Hope <www.420426962.com@qq.com>
2017-04-20 23:28:05 +0100

    show data in page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
+
 // var FoodItem=require('..models/fooditem')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	// var fooditems=FoodItem.find();
-  res.render('food/indexs', { title: 'Take away app' });
-  // res.render('food/indexs', { title: 'Take away app' ,fooditems:fooditems});
 
+	// var products=Product.find();
+	Product.find(function (err, docs) {
+        var fooditems = [];
+        var itemSize = 3;
+        for (var i = 0; i < docs.length; i += itemSize) {
+            fooditems.push(docs.slice(i, i + itemSize));
+        }
+        res.render('food/indexs', {title: 'Taking away web', products: fooditems});
+    });
 });
 
 module.exports = router;
